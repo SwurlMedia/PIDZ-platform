8d2b9eafe9222c196e93014c1eb94ff4b32e0039
Mick Jasker <mick@swurl.nl>
2020-05-29 12:55:17 +0200

    Add general grid

--- src/page/HomePage/HomePage.vue
+++ src/page/HomePage/HomePage.vue
@@ -2,30 +2,44 @@
 <script src="./HomePage.js"></script>
 
 <template>
-  <div :class="[$style.homePage]">
-    <h1>XXL Heading</h1>
-    <h2>XL Heading</h2>
-    <h3>L Heading</h3>
-    <h4>M Heading</h4>
-    <h5>S Heading</h5>
-    <h6>XXS Heading</h6>
-    <p>Maecenas faucibus mollis interdum. Donec sed odio dui. Sed posuere consectetur est at lobortis. Curabitur blandit tempus porttitor. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-    <BtnPrimaryFlat>Button Primary</BtnPrimaryFlat>
-    <BtnSecondaryFlat>Button Secondary</BtnSecondaryFlat>
-    <BtnLightFlat>Button Light</BtnLightFlat>
-    <TextField
-      label="Label"
-      placeholder="Placeholder"
-    />
-    <TextField
-      label="Password"
-      placeholder="Placeholder"
-      type="password"
-    />
-    <TextField
-      label="Textfield met CTA"
-      placeholder="Placeholder"
-      cta-copy="Versturen"
-    />
+  <div
+    :class="[$style.homePage]"
+    class="grid-container"
+  >
+    <section>
+      <h1>XXL Heading</h1>
+      <h2>XL Heading</h2>
+      <h3>L Heading</h3>
+      <h4>M Heading</h4>
+      <h5>S Heading</h5>
+      <h6>XXS Heading</h6>
+      <p>
+        Maecenas faucibus mollis interdum. Donec sed odio dui. Sed posuere consectetur est at
+        lobortis. Curabitur blandit tempus porttitor. Integer posuere erat a ante venenatis dapibus
+        posuere velit aliquet. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum
+        nibh, ut fermentum massa justo sit amet risus.
+      </p>
+    </section>
+    <section>
+      <BtnPrimaryFlat>Button Primary</BtnPrimaryFlat>
+      <BtnSecondaryFlat>Button Secondary</BtnSecondaryFlat>
+      <BtnLightFlat>Button Light</BtnLightFlat>
+    </section>
+    <section>
+      <TextField
+        label="Label"
+        placeholder="Placeholder"
+      />
+      <TextField
+        label="Password"
+        placeholder="Placeholder"
+        type="password"
+      />
+      <TextField
+        label="Textfield met CTA"
+        placeholder="Placeholder"
+        cta-copy="Versturen"
+      />
+    </section>
   </div>
 </template>
