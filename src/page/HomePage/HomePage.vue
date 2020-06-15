c14efeb17a71e7d673af0b91e5d11f9932cf96cf
Bart van Lier <bartvlier@gmail.com>
2020-06-15 13:05:09 +0200

    Changed dynamic data

--- src/page/HomePage/HomePage.vue
+++ src/page/HomePage/HomePage.vue
@@ -8,14 +8,14 @@
     <section>
       <Hero
         :cta="{to:'/', label:'Lees meer'}"
-        title="Ridiculus Adipiscing Vulputate Mattis"
+        title="Het coronavirus is niet weg, de curve helemaal platslaan is een illusie."
         image="https://images.unsplash.com/photo-1532938911079-1b06ac7ceec7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2689&q=80"
         spotlight
-        author="Kees Bovenkamp"
-        subject="Huisarts"
-        date="2 Uur Geleden"
+        author="Jantien Maassen"
+        subject="Covid-19"
+        date="Vrijdag 22 mei"
       >
-        Etiam porta sem malesuada magna mollis euismod. Sed posuere consectetur est at lobortis. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.
+        Bereid de IC-capaciteit structureel uit en wijs per regio een ziekenhuis aan "die covid doet", zegt Ernst Kuipers, verantwoordelijk voor de verdeling van corona-zorg in Nederland.
       </Hero>
     </section>
     <section>
