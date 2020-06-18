5a1caf0840ce66a2a361bcfb0ddcb35139fd8248
Mick Jasker <mick@swurl.nl>
2020-06-18 11:02:49 +0200

    Add 'wijPIDZ' logo

--- src/component/organism/Hero/Hero.vue
+++ src/component/organism/Hero/Hero.vue
@@ -8,6 +8,7 @@
     :style="{backgroundImage: `url('${ image }')`}"
   >
     <main>
+      <Icon name="wijPidz" :class="$style.logo" />
       <h2 v-if="spotlight">
         In de Spotlight
       </h2>
